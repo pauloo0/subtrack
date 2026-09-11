ef6b4f141651009d2d9b37c81b6f81ddbb94db4c
Paulo Oliveira <oliveirapaulo00@hotmail.com>
2026-09-11 21:22:21 +0100

    Separate sidebar from auth routes and authenticated routes

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,16 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  );
+}
